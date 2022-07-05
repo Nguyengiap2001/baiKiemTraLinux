f6ad7bbed7a47fb9b240b9c42d2a01789b0baaef
nguyengiap2001 <nguyengiap582001@gmail.com>
2022-07-05 16:12:51 +0700

    Initial comment

---
+++ 4f.sh
@@ -0,0 +1,16 @@
+echo "Nhap vao ten mot folder: "
+read DIR
+if [ -d "$DIR" ]; then
+  ### Take action if $DIR exists ###
+	for entry in `ls $DIR`; do
+	
+	    if [ ! -s $DIR/$entry ]
+		then
+			echo $entry
+		fi
+	done
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "Error: ${DIR} not found. Can not continue."
+  exit 1
+fi
